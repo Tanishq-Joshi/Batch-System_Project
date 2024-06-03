63bdfee78123aea546b880b0c889bf1102dedfc6
Tanishq-Joshi <tanishq.joshi28@gmail.com>
2024-06-03 11:58:19 +0530

    deleted:    public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/robots.txt 	deleted:    src/App.css 	deleted:    src/App.js 	new file:   src/App.jsx 	new file:   src/components/CSS/App.css 	new file:   src/components/CSS/navbar.css 	deleted:    src/logo.svg

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import "./components/CSS/App.css"
+import "./components/CSS/navbar.css"
+function Landing_page() {
+  return (
+
+    <div>
+       <div className="navbar">
+            <div className="navbar-container">
+            <a href="/"><img className="logo" src="https://i.ibb.co/cL3z6vT/Group.jpg" alt="Group"/></a>
+                <div style={{ display: "flex", gap: "5vh" }}>
+                    <a className="navbar-links" href="/">Home</a>
+                    <a className="navbar-links" href="/about">About Us</a>
+                    <a className="navbar-links" href="/pricing">Pricing </a>
+                    <a className="navbar-links" href="/features">Feature</a>
+                    
+                </div>
+            </div>
+            <button className="btn1">Download</button>
+        </div>
+     <div>      
+      <p id="head1">
+      <img id="p1" src="https://i.ibb.co/k3FGFSP/Group-35930.png" alt=""/>
+        Make The Best<br/> Financial Decisions
+      </p>
+      <p id="txt1">Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet<br/> faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.</p>
+      <button id="btn2">Get Started →</button>
+      <a href="/"><img id="vdo" src="https://i.ibb.co/f28tzg7/Group-4.png" /></a>
+
+            <img id="iphone" src="https://i.ibb.co/t8ByjKN/i-Phone-13-Pro-Front.png" alt="i-Phone-13-Pro-Front"/>
+            <img id="iphone2" src="https://i.ibb.co/DWF4LhW/i-Phone-13-Pro-Front-1.png" alt="i-Phone-13-Pro-Front-1"/>
+            <img id="iphone3" src="https://i.ibb.co/t8ByjKN/i-Phone-13-Pro-Front.png" alt="i-Phone-13-Pro-Front" />
+            <img id="iphone4" src="https://i.ibb.co/k3FGFSP/Group-35930.png" alt="i-Phone-13-Pro-Front"/>
+            <img id="circle" src="https://i.ibb.co/BZvY1Z5/Group-35887.png" alt="Group-35887" />
+            <img id="rib" src="https://i.ibb.co/wdpHK6J/Group-35924-1.png"/>
+    </div>
+
+
+    <div className="features">
+    <img id="pn" src="https://i.ibb.co/k3FGFSP/Group-35930.png" alt=""/>
+      <div>
+    <img id="iphone5" src="https://i.ibb.co/t8ByjKN/i-Phone-13-Pro-Front.png" alt="i-Phone-13-Pro-Front"/>
+    <img id="circle2" src="https://i.ibb.co/BZvY1Z5/Group-35887.png" alt="Group-35887" />
+    <p id="head2">FEATURES</p>
+    <h1 id="head3">Uifry Premium</h1>
+    </div>
+    <img id="star" src="https://i.ibb.co/sR7v0c7/star-05.png" alt="star-05" />
+    <p id="starp">Budgeting Intervals</p>
+    <p id="starpn">Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet <br/> faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.</p>
+    <img id="hex" src="https://i.ibb.co/dJ5KRq5/cube-02.png" alt="cube" />
+    <p id="hexp">Budgeting Intervals</p>
+    <p id="hexpn">Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet <br/> faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.</p>
+    <img id="cube" src="https://i.ibb.co/m9Gt0gf/cube-04.png" alt="cube-04" />
+    <p id="cubep">Budgeting Intervals</p>
+    <p id="cubepn">Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet <br/> faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.</p>
+    <img id="pnf" src="https://i.ibb.co/k3FGFSP/Group-35930.png" alt=""/>
+    </div>
+
+
+    <div>
+    <div>
+    <img id="iphone6" src="https://i.ibb.co/DWF4LhW/i-Phone-13-Pro-Front-1.png" alt="i-Phone-13-Pro-Front"/>
+    <img id="circle3" src="https://i.ibb.co/BZvY1Z5/Group-35887.png" alt="Group-35887" />
+    <p id="head4">ADVANTAGES</p>
+    <h1 id="head5">Why Choose Uifry ?</h1>
+    </div>
+    <img id="bell" src="https://i.ibb.co/wRdWV6R/Group-35899.png" alt="Group-35899" border="0"/>
+    <p id="bellp">Clever Notifications</p>
+    <p id="bellpn">Arcu at dictum sapien, mollis. Vulputate sit id accumsan, <br/>
+    ultricies. In ultrices malesuada elit mauris etiam odio. Duis<br/>
+     tristique lacus, et blandit viverra nisl velit. Sed mattis <br/>
+     rhoncus, diam suspendisse sit nunc, gravida eu. Lectus eget eget<br/>
+      ac dolor neque lorem sapien, suspendisse aliquam.</p>
+    </div>
+    <div>
+    <img id="iphone7" src="https://i.ibb.co/DWF4LhW/i-Phone-13-Pro-Front-1.png" alt="i-Phone-13-Pro-Front"/>
+    <img id="circle4" src="https://i.ibb.co/BZvY1Z5/Group-35887.png" alt="Group-35887" />
+    <h1 id="head6">Fully Customisable</h1>
+    <p id="starbn">Arcu at dictum sapien, mollis. Vulputate sit id accumsan, <br/>
+    ultricies. In ultrices malesuada elit mauris etiam odio. Duis<br/>
+     tristique lacus, et blandit viverra nisl velit. Sed mattis <br/>
+     rhoncus, diam suspendisse sit nunc, gravida eu. Lectus eget eget<br/>
+      ac dolor neque lorem sapien, suspendisse aliquam.</p>
+    </div>
+    <img id="starb"src="https://i.ibb.co/tqDSwfF/Group-35899-1.png" alt="Group-35899-1" border="0"/>
+    <div>
+    <img id="imgcard" src="https://i.ibb.co/DtCXnYP/Frame-1.png" alt="Frame-1" border="0"/>
+    <a href="/"><img id="dbtn" src="https://i.ibb.co/MstVwbQ/Group-2.png" alt="Group-2" border="0"/></a>
+    </div>
+    <div className="Footer">
+            <p>Copyright 2022 uifry.com all rights reserved</p>
+        </div>
+</div>
+
+  );
+}
+
+export default Landing_page;
